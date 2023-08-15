616c7aa21e8fa27db9b865b66273647d7d660c47
James McLaughlin <mclaughlinj.ny@gmail.com>
2023-08-15 15:05:30 -0400

    first function

--- functions.py
+++ functions.py
@@ -1,1 +1,6 @@
-
+def duplicate_protein_remover(protein_list):
+    blank_list=[]
+    for p in protein_list:
+        if p not in blank_list:
+            blank_list.append(p)
+    return blank_list
